317940d0e61bbae987788cc32097cda5cc36a853
Soroush Rasti <rasti.soroush@gmail.com>
2020-09-01 18:24:13 +0200

    uploading repo-2

--- scraping.py
+++ scraping.py
@@ -1,2 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+
+res=requests.get('https://news.ycombinator.com/news')
+soup=BeautifulSoup(res.text,'html.parser')
+
+
+links=soup.select('.storylink')
+votes=soup.select('.score')
+subtext=soup.select('.bubtext')
+
+def create_custome_hn(links,subtext):
+    hn=[]
+    for index,item in enumerate(links):
+        title=links[index].getText()
+        href=links[index].get('href',None)
+        vote=subtext[index].select('.score')
+        if len(vote):
+            points=int(vote[0].getText().replace(' points',''))
+            if points >99:
+                hh.append({'title':title,'link':href,'vote':points})
+    return hn
+
+
+
+import pprint
+
+pprint.pprint(create_custome_hn(links,subtext))
